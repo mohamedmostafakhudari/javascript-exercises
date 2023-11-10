6b661207bbda59c66d31938b4dae4a386a1ed8c9
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2023-11-10 10:26:46 +0200

    Complete 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removables) {
+	return array.filter((item) => !removables.includes(item));
 };
 
 // Do not edit below this line
