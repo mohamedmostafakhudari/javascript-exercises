df382706b8412a8875a93e6be32e3cdea859ab2a
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2023-11-10 10:58:36 +0200

    Complete 6th Exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+// The equation works as follows:
+// divide the year by 4
+// if it is fully divisible by 4, it is a leap year
+// But Century years need to be divided by 400 to be a leap year
+const leapYears = function (year) {
+	if (isCenturyYear(year)) {
+		return year % 400 === 0;
+	} else {
+		return year % 4 === 0;
+	}
+	function isCenturyYear(year) {
+		return year % 100 === 0;
+	}
 };
 
 // Do not edit below this line
