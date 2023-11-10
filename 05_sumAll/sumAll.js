4f6392d45ad7aa4320157e3b8f7282789a320076
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2023-11-10 10:45:24 +0200

    Complete 5th Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+	const errorCases = [!Number.isInteger(start), !Number.isInteger(end), start < 0, end < 0];
+	if (errorCases.includes(true)) return "ERROR";
+	let result = 0;
+	for (let currentNumber = Math.min(start, end); currentNumber <= Math.max(start, end); currentNumber++) {
+		result += currentNumber;
+	}
+	return result;
 };
 
 // Do not edit below this line
