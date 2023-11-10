b5d9a3219ed4eb5acaf8c07c8e351594304057ed
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2023-11-10 11:11:28 +0200

    Complete 7th Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+// Equation for converting from fahrenheit to celsius
+// (°F − 32) × 5/9 = °C
+const convertToCelsius = function (fahrenheit) {
+	return +Number.parseFloat((fahrenheit - 32) * (5 / 9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+// Equation for converting from celsius to fahrenheit
+// (°C × 9/5) + 32 = °F
+const convertToFahrenheit = function (celsius) {
+	return +Number.parseFloat(celsius * (9 / 5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
