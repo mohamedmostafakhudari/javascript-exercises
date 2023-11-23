87844fcfb8c487919d64cda5b93a1b5e358f8b98
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2023-11-23 23:00:40 +0200

    solve the 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	const editedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+	return editedString.split("").reverse().join("") === editedString;
 };
-
+palindromes("A car, a man, a maraca.");
 // Do not edit below this line
 module.exports = palindromes;
