03c749221c5f84d08f28ee46d395bde19a8a0714
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2023-11-23 22:32:03 +0200

    solve eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	if (num === 0) return 1;
+	return num * factorial(num - 1);
 };
-
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
