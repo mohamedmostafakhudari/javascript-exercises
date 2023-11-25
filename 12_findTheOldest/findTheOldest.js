6806630b0d4d3493bed05a78a1c2235aed6c4135
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2023-11-25 19:41:18 +0200

    Solve the 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	return people.reduce((oldest, curr) => {
+		if (!Object.keys(oldest).length) {
+			return curr;
+		}
 
+		const currAge = (curr?.yearOfDeath || new Date().getFullYear()) - curr.yearOfBirth;
+		const oldestAge = (oldest?.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+
+		return currAge > oldestAge ? curr : oldest;
+	});
 };
 
 // Do not edit below this line
