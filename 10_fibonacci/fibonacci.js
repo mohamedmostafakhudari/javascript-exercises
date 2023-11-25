cc442e1c32dd426e356e8decdd84e72c018e4b2a
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2023-11-25 17:41:23 +0200

    Solve 10th exercise (recursive solution)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibMap = new Map();
+const fibonacci = function (x) {
+	if (+x < 0) {
+		return "OOPS";
+	}
+	if (+x < 2) {
+		return +x;
+	}
+	if (fibMap.has(x)) {
+		return fibMap.get(x);
+	}
+	let fib0 = fibonacci(x - 2);
+	let fib1 = fibonacci(x - 1);
+	fibMap.set(x, fib0 + fib1);
+	return fib0 + fib1;
 };
 
 // Do not edit below this line
